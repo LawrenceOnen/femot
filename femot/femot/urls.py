484376a437409a173821f1b0cf6d4da9a970d34a
Lawrence Onen <lawrenceonen83@gmail.com>
2020-09-24 17:27:59 +0300

    added board data model

--- femot/femot/urls.py
+++ femot/femot/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from . import views
+from 
 
 urlpatterns = [
     url('^admin/', admin.site.urls),
-    #url(r'^users/', views.users, name='users'),
-    url(r'^boards/', views.boards, name='boards'),
+    url(r'^users/', views.users, name='users'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     #url(r'^topics/', views.topics, name='topics'),
     url(r'^$', views.index,),
 ]
